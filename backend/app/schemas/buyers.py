82098c2f9bdd27cf46fe68744ac2b3333611b1dd
Dinmukhamed Albek <dinmukhamedalbek@MacBook-Air-Dinmukhamed.local>
2024-10-07 10:05:29 +0500

    Remove unnecessary files and directories

--- backend/app/schemas/buyers.py
+++
@@ -1,8 +0,0 @@
-from pydantic import BaseModel
-
-
-class BuyerProfileCreate(BaseModel):
-    delivery_address: str
-
-    class Config:
-        schema_extra = {"example": {"delivery_address": "123 Street, City, Country"}}
